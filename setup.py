e6f54a52a49f7aef6d1da953a4cd4d7c96bb8146
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2018-04-29 14:35:21 +0900

    update setup.py to prepare pip installation

---
+++ setup.py
@@ -0,0 +1,32 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+requires = ["blockdiag"]
+
+VERSION = "0.0.1"
+setup(
+    name="pandoc_blockdiag_filter",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    version=VERSION,  # Ideally should be same as your GitHub release tag varsion
+    description="Yet another pandoc filters for blockdiag",
+    author="k4zuki",
+    author_email="k.yamamoto.08136891@gmail.com",
+    url="https://github.com/pandocker/pandoc-blockdiag-filter",
+    license="MIT",
+    install_requires=requires,
+    keywords=["pandoc", "markdown", "blockdiag"],
+    classifiers=["Development Status :: 4 - Beta",
+                 "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6",
+                 ],
+    python_requires=">=3.5,!=3.0.*,!=3.1.*,!=3.2.*",
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        "console_scripts": [
+            "pandocker-blockdiag-inline = blockdiag_filter.inline:main",
+            "pandocker-blockdiag = blockdiag_filter.block:main",
+        ],
+    },
+)
